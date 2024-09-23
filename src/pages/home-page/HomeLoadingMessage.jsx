d0be87327aba650ae33f8fdc0b24ff2aedd1b421
Jakub <derivatives_four@protonmail.com>
2024-09-23 12:59:34 +0200

    refactor: improve styling in `HomeLoadingMessage`
    - Added custom Tailwind classes which set component height on mobile and desktop devices.

--- src/pages/home-page/HomeLoadingMessage.jsx
+++ src/pages/home-page/HomeLoadingMessage.jsx
@@ -1,6 +1,15 @@
+import { isMobile } from "react-device-detect"
+import classNames from "classnames"
+
 const HomeLoadingMessage = () => {
+  const wrapperClass = classNames('flex flex-col items-center justify-center gap-2 text-2xl', {
+    'h-custom-max-mobile': isMobile,
+    'h-custom-max-desktop-mobile-lg': !isMobile
+  })
+
   return (
-    <div className="flex flex-col items-center gap-2 text-2xl">
+    // <div className="h-custom-max-mobile flex flex-col items-center justify-center gap-2 text-2xl">
+    <div className={wrapperClass}>
       <span>LOADING</span>
       <span className="loading loading-dots"></span>
     </div>
