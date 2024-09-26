40e6b0dbeccb9d7e8122bddbb7534291a89957bc
Jakub <derivatives_four@protonmail.com>
2024-09-26 09:21:31 +0200

    chore: move `Avatar.jsx` to `navbar` folder

--- src/components/navbar/Avatar.jsx
+++ src/components/navbar/Avatar.jsx
@@ -1,11 +1,11 @@
 // hook
-import useStateSelectors from "../../../hooks/useStateSelectors"
+import useStateSelectors from "../../hooks/useStateSelectors"
 
 const Avatar = () => {
   const {userData} = useStateSelectors()
 
   return (
-    <div className="w-12 rounded-full">
+    <div className="w-12 avatar rounded-full">
       <img
         alt="Avatar image"
         src={userData?.avatarUrl || 'https://res.cloudinary.com/dt5azfezg/image/upload/v1725642124/avatar_placeholder_sobov5.png'}
