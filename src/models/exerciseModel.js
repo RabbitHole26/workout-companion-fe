44949f44e60db11b19d863e6d1b5b5550fb27f81
Jakub <derivatives_four@protonmail.com>
2024-09-30 14:15:12 +0200

    refactor: improve properties types in `exerciseModel`
    - Converted `reps`, and `weight` in the Exercise class to type Number and parsed the value to integer.
      - This ensures passing correct data types to the BE when calling the relevant endpoints.
      - The change allows the Exercise class to be utilized in `useUpdateExercise` API hook.

--- src/models/exerciseModel.js
+++ src/models/exerciseModel.js
@@ -1,8 +1,10 @@
 class Exercise {
   constructor(title, reps, weight) {
     this.title = title.trim().toLowerCase()
-    this.reps = reps.trim().toLowerCase()
-    this.weight = weight.trim().toLowerCase()
+    // this.reps = reps.trim().toLowerCase()
+    // this.weight = weight.trim().toLowerCase()
+    this.reps = Number.parseInt(reps, 10)
+    this.weight = Number.parseInt(weight, 10)
   }
 }
 
