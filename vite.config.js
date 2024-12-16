aa69ca40dcbf0ddbb092b05fb4eb5f0e8910c00b
Jakub <derivatives_four@protonmail.com>
2024-12-16 12:49:41 +0100

    test: different proxy settings vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: process.env.VITE_BASE_URL,
+//         changeOrigin: true,
+//         secure: true,
+//         cookieDomainRewrite: {
+//           "*": "" // rewrite domain to enable cross-domain cookies
+//         }
+//       }
+//     }
+//   }
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_BASE_URL,
+        target: process.env.VITE_BASE_URL, // Replace with your backend URL
         changeOrigin: true,
         secure: true,
-        cookieDomainRewrite: {
-          "*": "" // rewrite domain to enable cross-domain cookies
-        }
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            proxyReq.setHeader('Origin', process.env.VITE_FRONTEND_URL); // Replace with your frontend URL
+          });
+        },
+      },
+    },
+  },
+});
